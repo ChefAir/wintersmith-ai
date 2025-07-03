7933fc727b80c4475aa22aa30aa8663b0dc29b89
ChefAir <infra@trillfamily.app>
2025-07-03 05:00:18 +0000

    updated src/lib/transporter.js to use a different port.

--- src/lib/transporter.js
+++ src/lib/transporter.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT) || 587,
-  secure: true,           // use STARTTLS
+  secure: false,           // use STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
